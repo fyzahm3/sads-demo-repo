ec4ab4ddd691c2af6e53e93c08f248132b09a814
Fayaz Ahmed <bfayazahmed2006@gmail.com>
2026-04-13 08:25:21 +0530

    new commit - with vul

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sqlite3
 import subprocess
 
 # Vulnerability 1: Hardcoded Secret
-
+password = "super_secret_123"
 
 def do_math(user_input):
     # Vulnerability 2: Eval
-
+    return eval(user_input)
 
 def get_user(username):
     cursor = sqlite3.connect('db.sqlite3').cursor()
     # Vulnerability 3: SQL Injection (f-string)
-
+    cursor.execute(f"SELECT * FROM users WHERE name = '{username}'")
